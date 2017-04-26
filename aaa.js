64b44dc90fc5fcd5435fb5199ae1d61dec737761
jonggyun <jonggyuni@gmail.com>
2017-04-26 17:26:10 +0900

    #연습중

--- aaa.js
+++ aaa.js
@@ -1,1 +1,5 @@
-alert("aaaaaa");
+const url = require('url');
+
+const result = url.parse('https://nodejs.org/dist/latest-v6.x/docs/api/url.html#url_url_parse_urlstring_parsequerystring_slashesdenotehost');
+
+console.log(result);
